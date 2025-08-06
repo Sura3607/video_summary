96b177b212084a756f9cafafcb49a247eddbe7d5
KhanhGiauTen <nqkhanh2925@gmail.com>
2025-08-06 21:51:25 +0700

    feat(formatter) : add format output for image function, rename format_output_video()

--- core/formatter.py
+++ core/formatter.py
@@ -5,8 +5,9 @@ import sys
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
 from core.videoinfo import VideoInfo
+from core.videoinfo import ImageInfo
 
-def format_output(video_id: VideoInfo) -> None:
+def format_output_video(video_id: VideoInfo) -> None:
     data = VideoInfo.get_data(self=video_id)
     if not data or "video_id" not in data or "chunks" not in data:
         raise ValueError("Dữ liệu video không hợp lệ")
@@ -41,3 +42,31 @@ def format_output(video_id: VideoInfo) -> None:
     
     with open(output_path, "w", encoding="utf-8") as f:
         json.dump(formatted_chunks, f, ensure_ascii=False, indent=2)
+        
+def format_output_image(image_info: ImageInfo) -> None:
+    data = image_info.get_data()
+    if not data or "image_id" not in data:
+        raise ValueError("Dữ liệu ảnh không hợp lệ")
+
+    if not data["vector"]:
+        raise ValueError("Không thể lưu vì thiếu vector")
+
+    if not data["summary"]:
+        print("Cảnh báo: Ảnh không có summary")
+
+    output_dir = "output"
+    os.makedirs(output_dir, exist_ok=True)
+    output_path = os.path.join(output_dir, f"{data['image_id']}.json")
+
+    formatted_data = {
+        "image_id": data["image_id"],
+        "transcript": data["transcript"],
+        "summary": data["summary"],
+        "keywords": data["keywords"],
+        "vector": data["vector"],
+        "frame": data["frame"],
+        "meta": data["meta"]
+    }
+
+    with open(output_path, "w", encoding="utf-8") as f:
+        json.dump(formatted_data, f, ensure_ascii=False, indent=2)
